9611fd66dda10802eb7d7c116dd0d1088f12312c
Isaac <isaacotero100@gmail.com>
2023-01-27 14:49:46 -0800

    Fixed issues with website not working, my work from codepen

--- actions.js
+++ actions.js
@@ -1,1 +1,32 @@
 document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+
+let currentIndex=0;
+
+const groups=document.getElementsByClassName("showoff-group");
+
+const handleYes =()=> {
+  
+  const nextInd=currentIndex+1 <= groups.length-1 ? currentIndex+1:0;
+
+  const currGroup= document.querySelector(`[data-index="${currentIndex}"]`);
+        nextGroup=document.querySelector(`[data-index="${nextInd}"]`);
+  
+  currGroup.dataset.status="after";
+  
+  nextGroup.dataset.status="active";
+  
+  currentIndex=nextInd;
+}
+const handleNo =()=> {
+  
+  const nextInd=currentIndex-1 >=0? currentIndex-1:groups.length-1;
+
+  const currGroup= document.querySelector(`[data-index="${currentIndex}"]`);
+        nextGroup=document.querySelector(`[data-index="${nextInd}"]`);
+  
+  currGroup.dataset.status="before";
+  
+  nextGroup.dataset.status="active";
+  
+  currentIndex=nextInd;
+}
